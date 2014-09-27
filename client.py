4fc3251eb7ce3f499f62ec5cb9e362d5d494f91c
Dexter Pratt <dexterpratt@Dexters-iMac.local>
2014-09-26 17:36:55 -0700

    start of class structure

--- client.py
+++ client.py
@@ -1,3 +1,35 @@
 #!/usr/bin/env python
 
 import requests
+import pandas as pd
+
+class Ndex:
+        
+    def __init__(self, host = "www.ndexbio.org", username = None, password = None):
+        self.host = host
+        self.username = username
+        self.password = password
+    
+# Base methods for making requests to this NDEx
+    
+    def put(self, route, params, payload):
+        url = self.host + route
+        
+    def post(self, route, params, payload):
+        url = self.host + route
+        
+    def delete(self, route):
+        url = self.host + route
+    
+    def get(self, route, params):
+        url = self.host + route
+        
+# Network methods
+        
+# User methods
+        
+# Group methods
+
+# Request methods
+
+# Task methods
